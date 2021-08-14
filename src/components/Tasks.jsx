f9b23d9071415c85f10d82754c09d1648240c647
cristiansuzuki <cristiansuzuki5@hotmail.com>
2021-08-14 03:13:41 -0300

    first commit

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Task from "./Task";
+
+const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task
+          task={task}
+          handleTaskDeletion={handleTaskDeletion}
+          handleTaskClick={handleTaskClick}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
